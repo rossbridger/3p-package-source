2717e20f11b321215e799c747a84a7a89bcaee56
Ross Charles C. <10781734+rossbridger@users.noreply.github.com>
2025-08-20 14:49:16 +0700

    Update Blast package to version 5.0.6 and change GitHub repository reference
    Signed-off-by: Ross Charles C. <10781734+rossbridger@users.noreply.github.com>

--- package-system/Blast/FindBlast.cmake
+++ package-system/Blast/FindBlast.cmake
@@ -13,66 +13,45 @@ if (TARGET ${TARGET_WITH_NAMESPACE})
 endif()
 
 set(_PACKAGE_DIR ${CMAKE_CURRENT_LIST_DIR}/Blast)
-
-set(${MY_NAME}_INCLUDE_DIR ${_PACKAGE_DIR}/sdk/common
-                           ${_PACKAGE_DIR}/sdk/extensions/assetutils/include
-                           ${_PACKAGE_DIR}/sdk/extensions/authoring/include
-                           ${_PACKAGE_DIR}/sdk/extensions/exporter/include
-                           ${_PACKAGE_DIR}/sdk/extensions/physx/include
-                           ${_PACKAGE_DIR}/sdk/extensions/serialization/include
-                           ${_PACKAGE_DIR}/sdk/extensions/shaders/include
-                           ${_PACKAGE_DIR}/sdk/extensions/stress/include
-                           ${_PACKAGE_DIR}/sdk/globals/include
-                           ${_PACKAGE_DIR}/sdk/lowlevel/include
-                           ${_PACKAGE_DIR}/sdk/toolkit/include)
-
-set(_LIBS_DIR ${_PACKAGE_DIR}/lib)
-set(_DLLS_DIR ${_PACKAGE_DIR}/bin)
-if(${PAL_PLATFORM_NAME} STREQUAL "Windows")
-    set(${MY_NAME}_LIBRARIES
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlast_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtAssetUtils_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtAuthoring_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtExporter_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtImport_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtPhysX_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtPxSerialization_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtSerialization_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtShaders_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtStress_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtTkSerialization_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastGlobals_x64.lib
-        ${_LIBS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastTk_x64.lib)
-
-    set(${MY_NAME}_RUNTIME_DEPENDENCIES
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlast_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtAssetUtils_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtAuthoring_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtExporter_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtPhysX_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtPxSerialization_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtSerialization_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtShaders_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtStress_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastExtTkSerialization_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastGlobals_x64.dll
-        ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/NvBlastTk_x64.dll
-    )
-    
-    # When building O3DE monolithicaly the library PhysXFoundation_64.dll won't be present since
-    # PhysX SDK uses a static version of it. Because of this Blast will provide the dll in this case, which
-    # is needed by its extended tools libraries NvBlastExtPhysX_x64.dll and NvBlastExtPxSerialization_x64.dll.
-    if(LY_MONOLITHIC_GAME)
-        list(APPEND ${MY_NAME}_RUNTIME_DEPENDENCIES
-            ${_DLLS_DIR}/vc15win64-cmake/$<LOWER_CASE:$<CONFIG>>/PhysXFoundation_64.dll
-        )
-    endif()
+if ($<LOWER_CASE:$<CONFIG>> STREQUAL "RELEASE")
+    set(${MY_NAME}_LIBRARY_DIR ${_PACKAGE_DIR}/release/bin)
+else()
+    set(${MY_NAME}_LIBRARY_DIR ${_PACKAGE_DIR}/debug/bin)
 endif()
+
+set(${MY_NAME}_INCLUDE_DIR ${_PACKAGE_DIR}/include/extensions/assetutils
+                           ${_PACKAGE_DIR}/include/extensions/authoring
+                           ${_PACKAGE_DIR}/include/extensions/authoringCommon
+                           ${_PACKAGE_DIR}/include/extensions/serialization
+                           ${_PACKAGE_DIR}/include/extensions/shaders
+                           ${_PACKAGE_DIR}/include/extensions/stress
+                           ${_PACKAGE_DIR}/include/globals
+                           ${_PACKAGE_DIR}/include/lowlevel
+                           ${_PACKAGE_DIR}/include/shared/NvFoundation
+                           ${_PACKAGE_DIR}/include/toolkit)
+
+set(IMPORTED_BLAST_LIBS_SUFFIX
+    NvBlastExtAssetUtils
+    NvBlastExtAuthoring
+    NvBlastExtSerialization
+    NvBlastExtShaders
+    NvBlastExtStress
+    NvBlastExtTkSerialization
+    NvBlastGlobals
+    NvBlast
+    NvBlastTk
+)
+
+foreach(BLAST_LIB ${IMPORTED_BLAST_LIBS_SUFFIX})
+    list(APPEND ${MY_NAME}_LIBRARIES ${${MY_NAME}_LIBRARY_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${BLAST_LIB}${CMAKE_STATIC_LIBRARY_SUFFIX})
+    list(APPEND ${MY_NAME}_RUNTIME_DEPENDENCIES ${${MY_NAME}_LIBRARY_DIR}/${CMAKE_SHARED_LIBRARY_PREFIX}${BLAST_LIB}${CMAKE_SHARED_LIBRARY_SUFFIX})
+endforeach()
 
 add_library(${TARGET_WITH_NAMESPACE} INTERFACE IMPORTED GLOBAL)
 ly_target_include_system_directories(TARGET ${TARGET_WITH_NAMESPACE} INTERFACE ${${MY_NAME}_INCLUDE_DIR})
 target_link_libraries(${TARGET_WITH_NAMESPACE} INTERFACE ${${MY_NAME}_LIBRARIES})
 target_compile_definitions(${TARGET_WITH_NAMESPACE} INTERFACE ${${MY_NAME}_COMPILE_DEFINITIONS})
+
 if(DEFINED ${MY_NAME}_RUNTIME_DEPENDENCIES)
     ly_add_target_files(TARGETS ${TARGET_WITH_NAMESPACE} FILES ${${MY_NAME}_RUNTIME_DEPENDENCIES})
 endif()
